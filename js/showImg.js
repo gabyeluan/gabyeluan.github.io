4eb5c171f68b1bf687b224609e24fa2ccd4cb7fc
Amon Silva <amon.bernardo@hotmail.com>
2019-01-28 19:47:47 -0200

    time faince done photos

--- js/showImg.js
+++ js/showImg.js
@@ -1,27 +1,33 @@
 $(function(){
-    $(".thumbernail").click(function(){
-        if ($(this).hasClass("thumbernail")){
-            $(".LargeImg").addClass("thumbernail");
-            $(".thumbernail").removeClass("LargeImg");
+    $('html, body').stop().animate({
+        scrollTop: $('#fotos').offset().top
+    }, 600);
 
-            $(this).addClass("LargeImg");
-            $(this).removeClass("thumbernail");
-            var $img = $(this);
-
-            //            console.log("Img top offset: " + $img.offset().top);
-            //            console.log("Window Top: " + ($img.offset().top - $(document).scrollTop()));
-            setTimeout(
-                function() 
-                {
-                    $('html, body').stop().animate({
-                        scrollTop: $img.offset().top
-                    }, 600);
-                }, 300);
-        }
-
-        else{
-            $(this).removeClass("LargeImg");
-            $(this).addClass("thumbernail");
-        }
-    }); 
 });
+//$(function(){
+//    $(".thumbernail").click(function(){
+//        if ($(this).hasClass("thumbernail")){
+//            $(".LargeImg").addClass("thumbernail");
+//            $(".thumbernail").removeClass("LargeImg");
+//
+//            $(this).addClass("LargeImg");
+//            $(this).removeClass("thumbernail");
+//            var $img = $(this);
+//
+//            //            console.log("Img top offset: " + $img.offset().top);
+//            //            console.log("Window Top: " + ($img.offset().top - $(document).scrollTop()));
+//            setTimeout(
+//                function() 
+//                {
+//                    $('html, body').stop().animate({
+//                        scrollTop: $img.offset().top
+//                    }, 600);
+//                }, 300);
+//        }
+//
+//        else{
+//            $(this).removeClass("LargeImg");
+//            $(this).addClass("thumbernail");
+//        }
+//    }); 
+//});
