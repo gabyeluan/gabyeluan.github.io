d90ddbe55826b3062859fc01fcc5c690c3572791
Amon Silva <35241797+Silva-Amon@users.noreply.github.com>
2019-12-26 19:49:34 -0300

    estão casados 2

--- js/countDown.js
+++ js/countDown.js
@@ -17,14 +17,14 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    //document.getElementById("countDown").innerHTML = days + "d " + hours + "h "
-    //    + minutes + "m " + seconds + "s ";
+    document.getElementById("countDown").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
     
     document.getElementById("countDown").innerHTML = "♥ Estamos casados!!! =D ♥"
 
     // If the count down is finished, write some text 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("countDown").innerHTML = "♥ Estamos casados!!! =D ♥";
     }
 }, 1000);
